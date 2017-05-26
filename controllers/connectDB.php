e42020ec8f533ff4e140688c3d39717e9d2fd2ff
Rafael Fronteira <rafael_fronteira@hotmail.com>
2017-05-25 23:40:24 -0300

    Update connectDB.php

--- controllers/connectDB.php
+++ controllers/connectDB.php
@@ -1,8 +1,10 @@
 <?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $bdName = "nome_do_bd";
+    //Código para conexão com o banco de dados mysql
+
+    $servidor = "localhost"; // nome do servidor
+    $usuario = "root"; // nome do usuario no mysql
+    $senha = ""; // senha do usuario no mysql
+    $bdName = "nome_do_bd"; // nome do banco de dados
     
     $conecta = new mysqli($servidor,$usuario,$senha,$bdName);
     
