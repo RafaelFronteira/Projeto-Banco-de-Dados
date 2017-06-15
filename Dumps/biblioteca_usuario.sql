2acfcaf7beea12c80733aca2451790721a02d2d6
Rafael Fronteira <rafael_fronteira@hotmail.com>
2017-06-15 12:38:33 -0300

    add banco de dados

---
+++ Dumps/biblioteca_usuario.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE  IF NOT EXISTS `biblioteca` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `biblioteca`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: biblioteca
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `cpf` varchar(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` int(8) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `celular` varchar(11) NOT NULL,
+  `fixo` varchar(10) NOT NULL,
+  `admin` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`cpf`),
+  UNIQUE KEY `cpf` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES ('02048375022','Gilnei Eduardo da S. Porto','e.porto@live.com',10070,'Estrada do Barro Vermelho, 241','993014444','5132224444',1),('02320202020','Malandro da Praia','malandro@beach.com',123234,'Rua da Praia 01','940123456','34345678',0),('02425506063','Daione C. B. Pavan','dayonne.p@gmail.com',10070,'Chapeu do Sol, 1522','995681522','5132456078',1),('65223225015','Odair C.Castro','odaircunhacastro@gmail.com',10070,'Acesso R4, 24','996413609','5132660988',1),('83162887020','Renata P. Cardoso','renata.pcardoso@hotmail.com',10070,'Beco 18, 12','983099086','5132486372',1),('85947245000','Rafael S. Fronteira','rsfronteira@gmail.com',10070,'Av. João Antônio Silveira, 1054','981309876','5132498588',1);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-14 18:08:05
